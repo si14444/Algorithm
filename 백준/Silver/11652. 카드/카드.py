f96f0930d79406a99058b4f697539b8bf9747820
BeomSu <75115874+si14444@users.noreply.github.com>
2024-09-17 23:39:06 +0900

    [Silver IV] Title: 카드, Time: 2452 ms, Memory: 55208 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.py
@@ -0,0 +1,10 @@
+n = int(input())
+d = {}
+for i in range(n):
+    card = int(input())
+    if card in d:
+        d[card] += 1
+    else:
+        d[card] = 1
+result = sorted(d.items(), key = lambda x: (-x[1], x[0]))
+print(result[0][0])
